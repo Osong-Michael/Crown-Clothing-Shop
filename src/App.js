796001a9fcd60aa6636e41532617c90a27d2b659
Osong Agberndifor <osongagberndifor@gmail.com>
2022-02-17 01:37:04 +0100

    Split up code into sections to handle different functions and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './HomePage';
+import HomePage from './Pages/HomePage';
 import './App.css';
 
 function App() {
---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../Components/Directory';
+import './index.scss'
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage
