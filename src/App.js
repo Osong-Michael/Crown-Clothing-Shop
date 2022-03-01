4f1ebbb2a2684ef7f1bdf123549a84518e5dc77f
Osong Agberndifor <osongagberndifor@gmail.com>
2022-03-01 17:25:21 +0100

    Added custom form input and button components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/Shop';
 import Header from './Components/Header';
+import AuthPages from './Pages/Authentication';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={AuthPages} />
       </Switch>
     </div>
   );
