942aaf2345d2f7741d2702279baf38f7100af6c6
Osong Agberndifor <osongagberndifor@gmail.com>
2022-03-01 16:23:07 +0100

    Added header component with some links

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/Shop';
+import Header from './Components/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
