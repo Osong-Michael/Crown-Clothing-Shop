53d252472aab6504c467ef3b8ae699e0dc333845
Osong Agberndifor <osongagberndifor@gmail.com>
2022-03-01 13:06:39 +0100

    Updated app with a shop page, collection preview and items components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './Pages/HomePage';
+import ShopPage from './Pages/Shop';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
