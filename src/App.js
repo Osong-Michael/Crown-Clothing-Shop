5aa5dfd6ec754b75dcc4d3ac6a9ec4a4950e0b97
Osong Agberndifor <osongagberndifor@gmail.com>
2022-03-03 04:06:49 +0100

    Added firestore db to handle storage of users on creation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/Shop';
 import Header from './Components/Header';
 import AuthPages from './Pages/Authentication';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends React.Component {
@@ -20,11 +20,22 @@ class App extends React.Component {
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log('USER: ', user);
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   };
 
   componentWillUnmount() {
