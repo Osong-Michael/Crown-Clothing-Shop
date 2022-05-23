44457377c562580afd0366f85d434927c16bdf32
Osong Agberndifor <osongagberndifor@gmail.com>
2022-05-23 14:34:12 +0100

    Added the UI and functionality for the cart dropdown box

--- src/Components/CartDropdown/index.jsx
+++ src/Components/CartDropdown/index.jsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react';
+import CartItem from '../CartItem';
+import { CartContext } from '../../Context/cartContext';
+
 import CustomButton from '../CustomButton';
 import './index.scss';
 
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
 
     return (
         <div className="cart-dropdown-container">
             <div className="cart-items">
-
-                <CustomButton>Go to checkout</CustomButton>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
+            <CustomButton>Go to checkout</CustomButton>
         </div>
     );
 };
--- src/Components/CollectionItem/index.jsx
+++ src/Components/CollectionItem/index.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../../Context/cartContext';
 import CustomButton from '../CustomButton';
 import './index.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">${price}</span>
+const CollectionItem = ({ product }) => {
+    const { name, price, imageUrl } = product;
+    
+    const { addItemsToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemsToCart(product);
+
+    return (
+        <div className="collection-item">
+            <div
+                className="image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">${price}</span>
+            </div>
+            <CustomButton buttonType='inverted' onClick={addProductToCart}>Add to Cart</CustomButton>
         </div>
-        <CustomButton buttonType='inverted'>Add to Cart</CustomButton>
-    </div>
-);
+    );
+
+};
 
 export default CollectionItem;
